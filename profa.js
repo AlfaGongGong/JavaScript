74e9662db8e52fb98c80730f3b77f8a944519796
Jasenko Bozinovic <64241482+sinner211@users.noreply.github.com>
2022-12-09 20:32:38 +0100

    09.12.22

---
+++ profa.js
@@ -0,0 +1,15 @@
+function korisnickiUnos() {
+    let unos = prompt("Unesi vrijednost sati i minuta 00-24");
+    if (unos >= 18 && unos <= 00) {
+        poruka = "dobro veče";}
+    else if (unos <= 18) {
+            poruka = "dobar dan";}
+        
+
+    }
+
+    // 
+
+
+
+
