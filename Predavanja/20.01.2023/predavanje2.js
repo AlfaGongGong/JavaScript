bc9a4488293937bb6f449d56573f4b564b679a2f
Archiel73 <unikat10@hotmail.com>
2023-01-20 20:32:05 +0100

    1

---
+++ Predavanja/20.01.2023/predavanje2.js
@@ -0,0 +1,8 @@
+function handleMouseOver(divElement) {
+
+  divElement.innerHTML = 'Još malo pa gotovo Bogu hvala';
+}
+function handleMouseOut(divElement) {
+
+  divElement.innerHTML = "Predavanje je počelo";
+}
