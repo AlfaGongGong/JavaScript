9b98a6211e93293d515fdf0c10d5c17dfbb80d39
Archiel73 <unikat10@hotmail.com>
2023-01-31 20:36:41 +0100

    3

--- Predavanja/20.01.2023/predavanje.js
+++ Predavanja/20.01.2023/predavanje.js
@@ -1,13 +1,1 @@
-function onBrojevniUnos() {
-  let brojevniUnos = brojevniUnosInput.value;
-  let message = ''
-  if (brojevniUnos == '') {
-    message = 'Niste unijeli ništa'
-  } else if (isNaN(brojevniUnos)) {
-    message = 'Niste unijeli broj'
-  } else if (brojevniUnos < 1 || brojevniUnos > 23) {
-    message = 'Unijeli ste broj veći od opsega 1 - 23'
-  } else {
-    message = 'Unijeli ste ispravan broj'
-  }
-}
+function onSubmitBroj() {}
